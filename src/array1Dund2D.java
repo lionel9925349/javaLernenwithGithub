68dcf710ea88d4318de95dea7855f73d4045e234
Ulrich Lionel Noumi Noumi <ulrich.lionel.noumi.noumi@mni.thm.de>
2022-08-02 09:46:49 +0200

    Initial commit

---
+++ src/array1Dund2D.java
@@ -0,0 +1,33 @@
+
+
+    public class array1Dund2D {
+        public static void main(String[] args) {
+            int [] array = new int[10];
+            int j = 0;
+            for (int i = 0; i < array.length; i++) {
+                j+=3;
+                array[i] = j;
+            }
+
+            for (int i = 0; i < array.length; i++) {
+                System.out.println("das ist meine Zahle " + array[i]);
+            }
+
+            int [][] array2D = new int[4][4];
+            array2D [0][0] = 2;
+            array2D [0][1] = 40;
+            array2D [3][1] = 35;
+            array2D [2][3] = 45;
+
+            for (int i = 0; i < array2D.length; i++) {
+                for (int k = 0; k < array2D[i].length; k++) {
+                    System.out.print( array2D[i][k] + "\t");
+
+                }
+                System.out.println();
+
+            }
+        }
+    }
+
+
