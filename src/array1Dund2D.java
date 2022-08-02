d02f49f0be29f5f4c11570741ca7ee9f9abb4df4
Ulrich Lionel Noumi Noumi <ulrich.lionel.noumi.noumi@mni.thm.de>
2022-08-02 09:55:04 +0200

    2dArrayErstellen

--- src/array1Dund2D.java
+++ src/array1Dund2D.java
@@ -2,6 +2,7 @@
 
     public class array1Dund2D {
         public static void main(String[] args) {
+            /*
             int [] array = new int[10];
             int j = 0;
             for (int i = 0; i < array.length; i++) {
@@ -22,6 +23,18 @@
             for (int i = 0; i < array2D.length; i++) {
                 for (int k = 0; k < array2D[i].length; k++) {
                     System.out.print( array2D[i][k] + "\t");
+
+                }
+                System.out.println();
+
+            }
+
+            */
+
+            int [][] nextArrsy2D = {{22,43,23},{23,44,54,23},{3,4}};
+            for (int i = 0; i < nextArrsy2D.length; i++) {
+                for (int k = 0; k < nextArrsy2D[i].length; k++) {
+                    System.out.print( nextArrsy2D[i][k] + "\t");
 
                 }
                 System.out.println();
